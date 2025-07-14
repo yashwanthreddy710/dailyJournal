27937b5c0d36a55b53c0dc19667d0ea878990a76
yashwanthreddy710 <yeshwanthreddy216@gmail.com>
2025-07-14 16:53:13 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -2,25 +2,27 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
+
 const authRoutes = require("./routes/authRoutes");
 const journalRoutes = require("./routes/journalRoutes");
-
-
 
 dotenv.config();
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "https://rainbow-cajeta-99bb26.netlify.app",
+    credentials: true,
+  })
+);
 
+// Middleware
+app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => res.send("API is running..."));
 app.use("/api/auth", authRoutes);
 app.use("/api/journals", journalRoutes);
-
 
 // Start Server
 const PORT = process.env.PORT;
@@ -29,7 +31,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(` Server running on port ${PORT}`);
     });
   })
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .catch((err) => console.error(" MongoDB connection error:", err));
+
